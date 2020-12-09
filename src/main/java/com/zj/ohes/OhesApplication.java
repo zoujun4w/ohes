38aaaa6eb3fef53553cca65892915d32bb5e40b6
zoujun <zoujun@oursvn.com>
2020-12-09 18:07:41 +0800

    first

---
+++ src/main/java/com/zj/ohes/OhesApplication.java
@@ -0,0 +1,13 @@
+package com.zj.ohes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhesApplication.class, args);
+    }
+
+}
